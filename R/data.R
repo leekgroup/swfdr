6d5c1082d076a88c9fb11cac652c99551ec0d8ce
SiminaB <simina@poisson>
2016-11-25 14:33:17 -0500

    Updated journal_pVals.RData, data.R

--- R/data.R
+++ R/data.R
@@ -16,3 +16,21 @@
 #' }
 #' @source \url{https://www.broadinstitute.org/collaboration/giant/index.php/GIANT_consortium_data_files#GWAS_Anthropometric_2015_BMI}
 "BMI_GIANT_GWAS_sample"
+
+#' P-values from abstracts from articles in 5 biomedical journals (American Journal of Epidemiology, BMJ, Jama, Lancet, New England Journal of Medicine), over 11 years (2000-2010).
+#'
+#' A dataset containing 15,653 p-values.
+#' 
+#'
+#' @format A data frame with 15,653 rows and 7 variables:
+#' \describe{
+#'   \item{pvalue}{P-value}
+#'   \item{pvalueTruncated}{Equals to 1 if the p-value is truncated, 0 otherwise}
+#'   \item{pubmedID}{Pubmed ID of the article}
+#'   \item{year}{Year of publication}
+#'   \item{abstract}{Abstract}
+#'   \item{title}{Title}
+#'   \item{journal}{Journal}
+#' }
+#' @source Code for extracting p-values at: \url{https://github.com/jtleek/swfdr/blob/master/getPvalues.R}
+
