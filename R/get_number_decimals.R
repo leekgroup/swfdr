e98b1d3bdc965bc97c4da55fe8cff8a9a031708a
Simina M. Boca <smb310@georgetown.edu>
2017-10-02 00:34:36 -0400

    Made some changes to get_number_decimals
    Still doesn't work properly...

--- R/get_number_decimals.R
+++ R/get_number_decimals.R
@@ -1,53 +1,45 @@
-#' Get number of decimals for any vector of numbers (i.e. return total number of digits after decimal point)
+#' Get number of decimals (i.e. return total number of digits after decimal point) for any vector of numbers in [0,1) if number of decimals <= 6
 #' 
-#' @param x Numerical vector
+#' @param x Numerical vector where all elements are in [0,1)
 #' 
-#' @return Vector giving the number of decimals for each element in x
+#' @return Vector giving the number of decimals for each element in x if the number is <= 6; otherwise return 7 with a warning
 #'
 #' @examples
-#' get_number_decimals(c(1,2))
 #' get_number_decimals(c(0.0006, 0.0750, 0.0420, 0.0031, 0.0001, 0.0100))
-#' get_number_decimals(c(6.5*10^-5, 0.0100))
-#' get_number_decimals(c(10.5,6.57*10^-5))
-#' get_number_decimals(c(-1000.05,-6.57*10^-5))
+#' get_number_decimals(c(6.5*10^-4, 0.0100)) ##This does not work correctly!
+#' get_number_decimals(c(6.5e-4, 0.0100))
+#' get_number_decimals(c(0.00065, 0.0100))
 #'
 #' @export
 get_number_decimals <- function(x)
 {
-  n_dec <- rep(NA, length=length(x))
+  if((any(x<0))|(any(x>=1)))
+  {
+    stop("All elements of x should be in [0,1)")
+  }
   
-  if((class(x) != "numeric") & (class(x) != "integer"))
-  {
-    stop("Input must be numeric")
+  ##get all numbers spaced 10^-k apart from 0 to 1
+  list_grid <- lapply(1:6, function(k){(1:10^k)/(10^k)})
+  
+  ##function for a single number
+  n_dec_single <- function(x_single){
+    ##get which vector the query number is in, which corresponds to the "number of digits"
+    grid_x_is_in <- sapply(list_grid, function(l,a){a %in% l}, x_single)
+    
+    if(x_single==0)
+    {
+      n_dec <- 0
+    } else {
+      if(sum(grid_x_is_in) >= 1)
+      {
+        n_dec <- min((1:6)[grid_x_is_in])
+      } else {
+        n_dec <- 7
+        warning("Number of decimals seems to be > 6. This case is not implemented. Beware floating point arithmetic!")
+      }
+    }
+    n_dec
   }
-  if(class(x) == "integer")
-  {
-    n_dec[] <- 0
-  } else {
-    ##convert to absolute value
-    x <- abs(x)
-    ##get rid of whole part 
-    x <- x-trunc(x)
-    
-    ##first convert into scientific notation
-    sci_note_x <- as.character(format(x,scientific = TRUE))
-    ##split on "e-"
-    split_on_e <- strsplit(sci_note_x, "e-")
-    ##get the negative exponents
-    neg_exp <- sapply(split_on_e, function(x){as.numeric(x[2])})
-    ##if NA, change to 0
-    neg_exp[is.na(neg_exp)] <- 0
-    
-    ##get the number of digits after "."
-    after_dot <- sapply(split_on_e, function(x){y <- strsplit(x[1], "\\.")[[1]][2]})
-    ##remove the "0" at the end
-    after_dot <- gsub("(0)+$","",after_dot)
-    after_dot <- nchar(after_dot)
-    after_dot[is.na(after_dot)] <- 0
-    
-    ##if NA, change to 0
-    n_dec <- after_dot + neg_exp
-  }
-  n_dec
+  n_d <- sapply(x, n_dec_single) 
+  n_d
 }
-
